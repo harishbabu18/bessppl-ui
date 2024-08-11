05b421251428f71ee8122d23c83a1d2e50391e70
harishbabu <harishbabu@harishbabus-iMac.local>
2024-08-11 23:11:02 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/vendor/bootstrap/css/bootstrap.min.css';
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
+import './assets/vendor/aos/aos.css';
+import './assets/vendor/glightbox/css/glightbox.min.css';
+import './assets/vendor/swiper/swiper-bundle.min.css';
+import './assets/css/main.css';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <Header />
+    <main className="main">
+   
+    </main>  
+    <Footer />
+  </>);
 }
 
 export default App;
